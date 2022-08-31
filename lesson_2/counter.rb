93ef84b2519bf34275c2c2abc3afc3ac29f11046
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-08-31 12:48:44 -0500

    Update server code and finish counter code

--- lesson_2/counter.rb
+++ lesson_2/counter.rb
@@ -4,7 +4,7 @@ def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(' ')
   path, params = path_and_params.split('?')
 
-  params = params.split('&').each_with_object({}) do |pair, hash|
+  params = (params || '').split('&').each_with_object({}) do |pair, hash|
     key, value = pair.split('=')
     hash[key] = value
   end
@@ -34,12 +34,14 @@ loop do
   client.puts params
   client.puts '</pre>'
 
-  client.puts '<h1>Rolls!</h1>'
-  rolls = params['rolls'].to_i
-  sides = params['sides'].to_i
+  client.puts '<h1>Counter</h1>'
 
-  rolls.times { client.puts '<p>', (rand(sides) + 1), '</p>' }
+  number = params['number'].to_i
 
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
   client.puts '</body>'
   client.puts '</html>'
 
