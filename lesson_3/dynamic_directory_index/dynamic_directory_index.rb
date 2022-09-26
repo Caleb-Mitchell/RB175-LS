f2d1f946a8a9d7957b613a36a8c701f405c5001a
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-09-26 16:17:24 -0500

    Complete Dynamic Directory Index challenge

--- lesson_3/dynamic_directory_index/dynamic_directory_index.rb
+++ lesson_3/dynamic_directory_index/dynamic_directory_index.rb
@@ -4,7 +4,9 @@ require 'tilt/erubis'
 
 get '/' do
   @title = 'Dynamic Directory Index'
+
   @files = Dir.glob("public/*").map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:order] == 'desc'
 
   erb :index
 end
