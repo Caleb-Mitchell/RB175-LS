c9aec31aa125b3457425a242047f22f926809528
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-09-27 12:58:57 -0500

    Add before filter to application code, to refactor redundant instance variable initialization

--- lesson_3/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer/book_viewer.rb
@@ -2,23 +2,21 @@ require 'tilt/erubis'
 require 'sinatra'
 require 'sinatra/reloader'
 
+before do
+  @contents = File.readlines('data/toc.txt')
+end
+
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines('data/toc.txt')
 
   erb :home
 end
 
 get '/chapters/:number' do
-  @contents = File.readlines('data/toc.txt')
-
   number = params[:number].to_i
+  chapter_name = @contents[number - 1]
 
-  # want to change the title displayed to the chapter name
-  # instead of the chapter number
-  # @title = "Chapter #{number}"
-
-  @title = "Chapter #{number}: #{@contents[number - 1]}"
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
